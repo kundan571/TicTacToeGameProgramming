6d177653231ebf9fcc6696026924b4830a031002
kundan571 <kundanchauhan1997@gmail.com>
2023-02-14 23:37:27 +0530

    Added: Method for choosing letter X or O

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -1,7 +1,38 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
+    public static char letter, player, computer;
+
+    //Method to give player chance to choose character X or O before starting game
+    public static char chooseLetterXorO() {
+        System.out.println("Choose a letter between x or o before starting game");
+        Scanner scanner = new Scanner(System.in);
+        letter = scanner.next().charAt(0);
+        return letter;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to TicTacToe Game Simulation");
+        char[] gameBoard = new char[10];
+        for (int i = 1; i < 10; i++) {
+            gameBoard[i] = (char) (-+'0');
+        }
+        System.out.println("Welcome to TicTacToe game");
+        for (int i = 1; i < 10; i++) {
+            if (i == 4 || i == 7) {
+                System.out.println();
+            }
+            System.out.print(gameBoard[i] + " ");
+        }
+        System.out.println();
+        //player choose a letter between x or o to proceed
+        player = chooseLetterXorO();
+        if (player == 'x') {
+            computer = 'o';
+        } else {
+            computer = 'x';
+        }
+
     }
 }
